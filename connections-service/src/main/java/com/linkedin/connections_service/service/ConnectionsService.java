d91696398f696c3957f2f3a17695b317f87d3543
Ujjwal-0 <kumarujjwal7349@gmail.com>
2026-03-15 10:54:14 +0530

    Added auth-service and integrated JWT authentication across services

--- connections-service/src/main/java/com/linkedin/connections_service/service/ConnectionsService.java
+++ connections-service/src/main/java/com/linkedin/connections_service/service/ConnectionsService.java
@@ -1,5 +1,6 @@
 package com.linkedin.connections_service.service;
 
+import com.linkedin.connections_service.auth.UserContextHolder;
 import com.linkedin.connections_service.entity.Person;
 import com.linkedin.connections_service.repository.PersonRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,7 +16,8 @@ public class ConnectionsService {
 
     private final PersonRepository personRepository;
 
-    public List<Person> getFirstDegreeConnections(Long userId) {
+    public List<Person> getFirstDegreeConnections() {
+        Long userId = UserContextHolder.getCurrentUserId();
         log.info("Getting first degree connections for user with id: {}", userId);
 
         return personRepository.getFirstDegreeConnections(userId);
